af20a05418093e4db8dbc4e8b489467fbfa9dfbf
Zac McCormick <zac.mccormick@gmail.com>
2024-03-24 23:03:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Fulcrum AI Resources
+
+## Overview
+
+Computer Vision in Fulcrum is exposed by the `INFERENCE()` function in Data Events. Using a combination of Fulcrum features, we can load and run inference on photos taken inside the app. Trained models are uploaded as Reference Files on an app. Fulcrum supports models in the [ONNX Runtime `.ort`](https://onnxruntime.ai/) format. It supports some flexibility with input tensor formats to accommodate models trained from the Tensorflow ecosystem or the PyTorch ecosystem.
+
+Below is the basic process of preparing a model to use in Fulcrum:
+
+* Train or find a model in either PyTorch .pt format or Tensorflow format
+* Convert it to an ONNX model (`.onnx` file)
+* Convert the `.onnx` file to an `.ort` file for use in the ONNX Runtime and Fulcrum
+* Upload the `.ort` file as a reference file
+* Upload the labels `.txt` file as a reference file
+* Add a data events snippet to run the inference
+
+## Example Models
+
+* [Utility Poles](https://drive.google.com/file/d/1sISnmO4TRAqm4DBgLtKeaKiFcB2a75Kx/view?usp=sharing) [labels](https://drive.google.com/file/d/1ADzWi5QLJLJbtrIyhCnr81CziNskfQW1/view?usp=drive_link)
+* [YOLOv5 Object Detection](https://drive.google.com/file/d/1VZ7OJrRIivsFGYQaPBgb6O10qubKg3E2/view?usp=drive_link) [labels](https://drive.google.com/file/d/1WfA-O2RTjogKZqi9WKwj1zvIkGY6o9ko/view?usp=drive_link)
+* [YOLOv5 Classification](https://drive.google.com/file/d/1UO_rDDowGj5BnFqKooTkE1_Su5WGhilF/view?usp=drive_link) [labels](https://drive.google.com/file/d/1OIDh6fX702tzHHf3mIb62nUnx5ZHfLDr/view?usp=sharing)
+
+## Model Outputs
+
+Processing model output is the most complex part of integrating a custom model. Each model architecture generally has its own output shape and processing requirements. And depending on how the model was created, it may or may not have embedded post processing like NMS (non max suppression) which is required for practical use of the model. For object detection models that require NMS, it must currently be done inside the model with the [`NonMaxSuppression`](https://github.com/onnx/onnx/blob/main/docs/Operators.md#NonMaxSuppression) operator at the end. Netron is a useful tool for inspecting the model inputs and outputs.
+
+## Helpers
+
+In this repo there is an `ai.js` file which can be added as a Reference File and loaded dynamically to access some helper functions. This will help during rapid prototyping to share some code while models are developers and we learn how to generalize and simplify model input handling and output post-processing.
+
+## Training a custom model
+
+* [Notebook for training a YOLOv5 object detector](https://colab.research.google.com/drive/1DlDVnYTftdAZ83SkUXTEXAO4utp2h0Eu?usp=sharing)
---
+++ ai.js
@@ -0,0 +1,228 @@
+function runInference(
+  {
+    photo_id,
+    form_id,
+    form_name,
+    model,
+    size,
+    format,
+    type,
+    mean,
+    std
+  }, 
+  callback
+) {
+  INFERENCE({
+    photo_id: photo_id,
+    form_id: form_id == null && form_name == null ? FORM().id : null,
+    form_name: form_name,
+    model,
+    size,
+    format: format ?? 'hwc',
+    type: type ?? 'float',
+    mean,
+    std,
+  }, (error, result) => {
+    if (error) {
+      ALERT(error.message);
+      return;
+    }
+
+    for (const output of Object.values(result.outputs)) {
+      if (Array.isArray(output.value)) {
+        output.value = FLATTEN(output.value);
+      }
+    }
+
+    if (callback) {
+      callback(error, result);
+    }
+  });
+}
+
+const YOLOv5 = {};
+
+YOLOv5.runClassification = function(
+  {
+    photo_id,
+    form_id,
+    form_name,
+    model,
+    size,
+    format,
+    type,
+    mean,
+    std,
+    labels,
+    threshold,
+    top
+  },
+  callback
+) {
+  runInference({
+    photo_id,
+    form_id,
+    form_name,
+    model: model ?? 'yolov5m-cls.ort',
+    size: size ?? 224,
+    format: format ?? 'chw',
+    type: type ?? 'float',
+    mean: mean === null ? null : [0.485, 0.456, 0.406],
+    std: std === null ? null : [0.229, 0.224, 0.225]
+  }, (error, results) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+
+    const processed = YOLOv5.processClassificationOutput({
+      values: Object.values(results.outputs)[0].value,
+      labels,
+      threshold,
+      top
+    });
+
+    callback(null, processed);
+  });
+}
+
+YOLOv5.processObjectDetectionOutput = function({
+  detectionBoxes,
+  detectionClasses,
+  detectionScores,
+  detectionCount,
+  labels,
+  threshold
+}) {
+  const resultsArray = [];
+  const count = detectionCount.length > 0 ? detectionCount[0] : 0;
+
+  if (count === 0) {
+    return resultsArray;
+  }
+
+  for (let i = 0; i < count; ++i) {
+    const score = detectionScores[i];
+
+    // Filter results with score < threshold.
+    if (score < threshold) {
+      continue;
+    }
+
+    const classIndex = detectionClasses[i];
+    const className = labels[classIndex];
+
+    const x1 = detectionBoxes[4 * i];
+    const y1 = detectionBoxes[4 * i + 1];
+    const x2 = detectionBoxes[4 * i + 2];
+    const y2 = detectionBoxes[4 * i + 3];
+
+    const box = {
+      x: x1,
+      y: y1,
+      width: x2 - x1,
+      height: y2 - y1
+    };
+
+    resultsArray.push({
+      className,
+      classIndex,
+      score,
+      box
+    });
+  }
+
+  return resultsArray.sort((first, second) => {
+    return first.score - second.score;
+  });
+}
+
+YOLOv5.processClassificationOutput = function({
+  values,
+  labels,
+  threshold,
+  top
+}) {
+  const results = values.map((score, index) => {
+    return {
+      index,
+      score,
+      label: labels[index]
+    };
+  });
+
+  const sorted = results.sort((a, b) => a.score - b.score).reverse();
+
+  const topK = top != null ? sorted.slice(0, top) : sorted;
+
+  return topK.filter(result => result.score > (threshold ?? 0));
+}
+
+YOLOv5.runObjectDetection = function(
+  {
+    photo_id,
+    form_id,
+    form_name,
+    model,
+    size,
+    format,
+    type,
+    labels,
+    threshold
+  },
+  callback
+) {
+  runInference({
+    photo_id,
+    form_id,
+    form_name,
+    model: model ?? 'yolov5m.ort',
+    size: size ?? 640,
+    format: format ?? 'hwc',
+    type: type ?? 'float'
+  }, (error, { outputs }) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+
+    const detections = YOLOv5.processObjectDetectionOutput({
+      detectionBoxes: outputs.output_0.value,
+      detectionScores: outputs.output_1.value,
+      detectionClasses: outputs.output_2.value,
+      detectionCount: outputs.output_3.value,
+      labels: labels,
+      threshold: threshold ?? 0.3
+    });
+
+    callback(null, detections);
+  });
+}
+
+function chatGPT({ prompt, apiKey, model, temperature }, callback) {
+  const options = {
+    method: 'POST',
+    url: 'https://api.openai.com/v1/chat/completions',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+     "model": model ?? 'gpt-3.5-turbo',
+     "messages": [{ role: "user", content: prompt }],
+     "temperature": temperature ?? 0.7
+    })
+  };
+
+  REQUEST(options, (req, res, body) => {
+    const json = JSON.parse(body);
+
+    callback(json.choices && json.choices.length ? json.choices[0].message.content : null, json);
+  });
+}
+
+module.exports = {
+  runInference,
+  YOLOv5,
+  chatGPT
+};
